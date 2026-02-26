172416f67e38bb6a19c454a4a649f0f91acb855f
enriquecamargo <2024030456@upsin.edu.mx>
2026-02-25 20:15:21 -0700

    Start of Springboot basic course

---
+++ Springboot-basic/src/main/java/com/SpringBoot_basic/Springboot_basic/SpringbootBasicApplication.java
@@ -0,0 +1,15 @@
+package com.SpringBoot_basic.Springboot_basic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBasicApplication.class, args);
+
+	}
+
+
+}
---
+++ Springboot-basic/src/test/java/com/SpringBoot_basic/Springboot_basic/SpringbootBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringBoot_basic.Springboot_basic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
